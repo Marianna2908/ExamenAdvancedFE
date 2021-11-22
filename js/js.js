f81c68522a0e6e2d4e2632d653d356062c29447f
Marianna <mar-ivanov94@hotmail.com>
2021-11-22 11:25:16 +0100

    examen vraag 2

---
+++ js/js.js
@@ -0,0 +1,34 @@
+class Merk{
+    constructor(naam) {
+        this.naam= naam;
+    }
+}
+
+class Soort{
+    constructor(geslacht) {
+        this.geslacht=geslacht;
+    }
+}
+class Kleur{
+    constructor(benamingKleur) {
+        this.benamingKleur;
+    }
+}
+class Size{
+    constructor(afmetingWiel) {
+        this.afmetingWiel= afmetingWiel;
+    }
+}
+class Acties{
+    constructor(beweging, uitvoering) {
+        this.beweging= beweging;
+        this.uitvoering= uitvoering;
+    }
+}
+class Snelheid{
+    constructor(snelheid) {
+        this.snelheid= snelheid;
+    }
+}
+
+
